db13038b63ce1de5fb0773539766056960ec670e
Yexiaomo <markdownpad@163.com>
2018-06-25 15:14:50 +0800

    update

--- autoGitPull.py
+++ autoGitPull.py
@@ -3,6 +3,4 @@ import os
 os.chdir('/var/www/blog-yexiaomo/')
 os.system('date >> autoGitPull.log')
 os.system('sudo git pull origin master')
-os.chdir('/var/www/blog-yexiaomo/')
-os.system('sudo hexo clean')
-os.system('sudo hexo d -g')
+os.system('sudo hexo clean && sudo hexo d -g')
