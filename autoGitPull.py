60d41aea7014fa2245ed5d13b149e9033c24c150
Yexiaomo <markdownpad@163.com>
2018-06-28 11:20:52 +0800

    完善代码

--- autoGitPull.py
+++ autoGitPull.py
@@ -3,4 +3,4 @@ import os
 os.chdir('/var/www/blog-yexiaomo/')
 os.system('date >> autoGitPull.log')
 os.system('sudo git pull origin master')
-os.system('sudo hexo clean && sudo hexo d -g')
+os.system('sudo hexo clean && sudo hexo g')
